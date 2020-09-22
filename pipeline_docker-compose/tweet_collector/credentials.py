13e0dc2696033420746318caf7cdb011404f38f3
laraschmitt <lara_schmitt@posteo.de>
2020-09-22 20:18:08 +0200

    update cred2

--- pipeline_docker-compose/tweet_collector/credentials.py
+++ pipeline_docker-compose/tweet_collector/credentials.py
@@ -1,4 +1,4 @@
-#Spiced Twitter Credentials you can use (should work) if you don't have a Twitter acct.
+# File containing Twitter credentials
 CONSUMER_API_KEY = "<consumer_api_key>"
 CONSUMER_API_SECRET = "<consumer_api_secret>"
 ACCESS_TOKEN = "<access_token>"
