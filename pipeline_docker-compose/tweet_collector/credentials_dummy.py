da2628f986d649aca982912d970f59623ccbf658
laraschmitt <lara_schmitt@posteo.de>
2020-09-22 20:24:21 +0200

    update

---
+++ pipeline_docker-compose/tweet_collector/credentials_dummy.py
@@ -0,0 +1,5 @@
+# File containing Twitter credentials
+CONSUMER_API_KEY = "<consumer_api_key>"
+CONSUMER_API_SECRET = "<consumer_api_secret>"
+ACCESS_TOKEN = "<access_token>"
+ACCESS_TOKEN_SECRET = "access_token_secret"
